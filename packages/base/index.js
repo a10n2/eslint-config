435db81d63b1459d21f9bc6a883a886bf6efea94
a10n2-dev <yankit.alone@gmail.com>
2023-02-07 23:07:10 +0800

    初步设置好base相关js的eslint.rules

--- packages/base/index.js
+++ packages/base/index.js
@@ -3,12 +3,34 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    es6: true
   },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: [],
-  rules: [],
-  overfides: [],
+  rules: {
+    // warn
+    'no-unuse-vars': 'warn',
+    'default-case': 'warn',
+    'no-alert': 'warn',
+    // error
+    'eqeqeq': 'error',
+    'no-eval': 'error',
+    'no-var': 'error',
+    'no-await-in-loop': 'error',
+    'no-duplicate-imports': 'error',
+    'no-use-before-define': 'error',
+    'default-case-lastst': 'error',
+    'arrow-body-style': ['error', 'never'],
+    // style warn
+    'block-spacing': ['error', 'always'],
+    'func-call-spacing': ["error", "never"],
+    'max-len': ['error', { "code": 100, 'tabWidth': 2, "comments": 65 }],
+    // use 2-space
+    'indent': ['error', 2],
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    "semi": ["error", "never"]
+  }
 }
