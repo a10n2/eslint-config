bae6432a09b9f323aa82bcad752278de9fab5874
a10n2-dev <yankit.alone@gmail.com>
2023-02-08 23:27:18 +0800

    更新 packages/ts 的一些规则

--- packages/ts/index.js
+++ packages/ts/index.js
@@ -1,3 +1,24 @@
 module.exports = {
-  extends: '@a10n2/eslint-config-base',
+  extends: [
+    '@a10n2/eslint-config-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // warn
+    '@typescript-eslint/no-explicit-any': 'warn',
+    // error
+    '@typescript-eslint/member-ordering': [
+      'error',
+      { default: ['signature', 'method', 'constructor', 'field'] },
+    ],
+    '@typescript-eslint/no-duplicate-enum-values': 'error',
+    '@typescript-eslint/no-import-type-side-effects': 'error',
+  },
 }
