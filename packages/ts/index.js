c65997398c3b2d8ce90de3321e4869575c22f0e6
a10n2-dev <yankit.alone@gmail.com>
2023-02-09 11:24:16 +0800

    update pakcages/ts config content

--- packages/ts/index.js
+++ packages/ts/index.js
@@ -5,10 +5,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
-  },
   plugins: ['@typescript-eslint'],
   rules: {
     // warn
